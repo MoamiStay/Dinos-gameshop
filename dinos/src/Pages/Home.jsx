44af2f45bff06f294d9b59379e930814dd65cc1f
Regine <reginenaas@gmail.com>
2024-06-04 06:45:15 +0200

    Working on fetching API data
    Testing data extraction for homepage

--- dinos/src/Pages/Home.jsx
+++ dinos/src/Pages/Home.jsx
@@ -1,10 +1,49 @@
+import useApi from "../Hooks/useApi";
+const UrlGameList = "https://api.rawg.io/api/games?page=1&key=0a4e4684ac2e467782d960abdd0667a2";
 
 const Home = () => {
+  const { data, isLoading, isError } = useApi(UrlGameList)
+  // const games = JSON.stringify(data);
+  // console.log(data.results);
+  // console.log(Object.keys(data));
+
+  let games = data.results;
+  console.log(games);
+  console.log(Object.keys(games));
+
+  if (isLoading) {
   return (
     <>
-      <p>Hello from Home</p>
+      <p>Loading...</p>
     </>
   );
+  }
+
+    if (isError) {
+  return (
+    <>
+      <p>An error occured while fetching games</p>
+    </>
+  );
+  }
+
+
+  return (
+    <>
+    <h1>Successfully found data HURRAY</h1>
+    <div>
+      {/* {Object.keys(games).map((item, idx) => {
+        return <>{idx}</>
+      })} */}
+      {/* {games.map((item, idx) => {
+        return <p key={idx}> {item}</p>
+      })} */}
+    </div>
+    </>
+  )
+
+
+
 };
 
 export default Home;
