81aaf02f79700361214cb273e78898010c5be3ed
Regine <reginenaas@gmail.com>
2024-06-05 03:12:13 +0200

    Got data
    Received game titles and displayed to page

--- dinos/src/Pages/Home.jsx
+++ dinos/src/Pages/Home.jsx
@@ -3,13 +3,8 @@ const UrlGameList = "https://api.rawg.io/api/games?page=1&key=0a4e4684ac2e467782
 
 const Home = () => {
   const { data, isLoading, isError } = useApi(UrlGameList)
-  // const games = JSON.stringify(data);
-  // console.log(data.results);
-  // console.log(Object.keys(data));
+  const games = data.results;
 
-  let games = data.results;
-  console.log(games);
-  console.log(Object.keys(games));
 
   if (isLoading) {
   return (
@@ -27,22 +22,17 @@ const Home = () => {
   );
   }
 
+let doc = "";
 
-  return (
-    <>
-    <h1>Successfully found data HURRAY</h1>
-    <div>
-      {/* {Object.keys(games).map((item, idx) => {
-        return <>{idx}</>
-      })} */}
-      {/* {games.map((item, idx) => {
-        return <p key={idx}> {item}</p>
-      })} */}
-    </div>
-    </>
-  )
-
-
+if (games) {
+for (let game in Object.keys(games)) {
+  console.log(games[game].name);
+  doc += `
+  ${games[game].name}
+  `
+};
+return doc;
+}
 
 };
 
